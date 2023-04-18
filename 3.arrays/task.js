0dc60ef6521e31f9eca430c42e3cc6d3a0c82956
MainArmyn <mainarmyn@gmail.com>
2023-04-18 20:00:21 +0400

    Реализовал третье домашние задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    let result=arr1.every((item,index,arr) => item===arr2[index] && arr.length===arr2.length);
+    return result;
+  }
+  function getUsersNamesInAgeRange(users, gender) {
+    let result=users.filter(user => user.gender===gender).reduce((acc,el,index,arr) => {
+      acc=acc+el.age;
+      if (index===arr.length-1) {
+        return acc/arr.length;
+      } else {
+        return acc;
+      }
+    }, 0);
+    return result;
+  }
