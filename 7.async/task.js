fbe367ed969548adfc59d0ec9879359f925175f6
MainArmyn <126054755+MainArmyn@users.noreply.github.com>
2023-05-01 14:15:43 +0400

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -6,18 +6,12 @@ class AlarmClock {
     addClock(time,func) {
       if (time===null || func===undefined) {
         throw new Error('Отсутствуют обязательные аргументы');
-      } else {
-        if (this.alarmCollection.length===0) {
+      } 
+        if (this.alarmCollection.some(item => item.time===time)) {
           this.alarmCollection.push({callback: func,time: time,canCall:true});
-        } else {
-          for (let clock of this.alarmCollection) {
-            if (clock.time===time) {
-              console.warn('Уже присутствует звонок на это же время');
-            }
-          }
-          this.alarmCollection.push({callback: func,time: time,canCall:true});
+          console.warn('Уже присутствует звонок на это же время');
         }
-      }
+        this.alarmCollection.push({callback: func,time: time,canCall:true});
     }
     removeClock(time) {
       this.alarmCollection=this.alarmCollection.filter(item => item.time!==time );
@@ -31,14 +25,13 @@ class AlarmClock {
     start() {
       if (this.intervalId) {
         return;
-      } else {
+      }
         this.alarmCollection.forEach(item => {
         if (item.time===this.getCurrentFormattedTime() && item.canCall===true) {
           item.canCall=false;
           this.intervalId=setInterval(item.callback);
       } 
       });
-    }
   }
     stop() {
       clearInterval(this.intervalId);
