625be4bb6b1cbc893e027c71d57630457cbdf6fa
MainArmyn <126054755+MainArmyn@users.noreply.github.com>
2023-05-02 12:16:08 +0400

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+    let cashe={};
+    let count=0;
+    return (...args) => {
+      const hash=args.join(',');
+      if (count===5) {
+        delete cashe[Object.keys(cashe)[0]];
+        if (hash in cashe) {
+            console.log(Object.keys(cashe));
+            return "Из кэша: "+cashe[hash];
+          }
+        const result=func(...args);
+        count-=1;
+        cashe[hash]=result;
+        console.log(Object.keys(cashe));
+        return "Вычисляем: "+result;
+      } 
+      if (hash in cashe) {
+        console.log(Object.keys(cashe));
+        return "Из кэша: "+cashe[hash];
+      }
+      const result=func(...args);
+      cashe[hash]=result;
+      count+=1;
+      console.log(Object.keys(cashe));
+      return "Вычисляем: "+result;
+    }
+  }
+  function debounceDecoratorNew(func,delay) {
+    let isThrottled=false;
+    function wrapper(...args) {
+      wrapper.allCount+=1;
+      if (isThrottled) {
+        return;
+      }
+      let result=func(...args);
+      wrapper.count+=1;
+      isThrottled=true;
+      setTimeout(() => isThrottled=false,delay);
+      return result;
+    }
+    wrapper.count=0;
+    wrapper.allCount=0;
+    return wrapper;
+  }
